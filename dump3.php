55d1c57e825594d48a2f94f0be0abb246f8f55d2
Arvial <59569317+Arvialo@users.noreply.github.com>
2021-04-11 21:36:05 +0200

    Add files via upload

---
+++ dump3.php
@@ -0,0 +1,55 @@
+<?php
+
+$pass = "@rtHur!Pass1234";
+$user= "arvialo";
+$host = "localhost";
+$name= "temp";
+
+$pdo = new PDO("mysql:host=".$host.";dbname=".$name,$user,$pass);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+if (isset($_GET['req'])){
+    $req = $pdo->query($_GET['req'])->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($req);
+
+}
+else
+{ 
+?>
+<html>
+<input type="text" id="req" name="request" placeholder="SQL">
+<div id="output">
+
+</div>
+
+<script>
+var req = document.querySelector("#req");
+var output = document.querySelector("#output");
+req.addEventListener('input', () => {
+
+        fetch(`dump3.php?req=${req.value}`)
+        .then(resp => {
+            if (resp.status == 500){
+                output.innerHTML = "Invalid Request";
+            }
+            else {
+                resp.json().then(
+                    data => {
+                        output.innerHTML = "";
+                        console.log(data);
+                        data.forEach(elem => {
+                            for (const key in elem){
+                                output.innerHTML += `<h1>${key}</h1>------<h3>${elem[key]}</h3>`;
+                            }
+                        })
+                    }
+                );
+            }
+        })
+
+});
+</script>
+
+</html>
+<?php } ?>
---
+++ dump4.php
@@ -0,0 +1,45 @@
+<?php
+
+$pass = "@rtHur!Pass1234";
+$user= "arvialo";
+$host = "localhost";
+$name= "temp";
+
+$pdo = new PDO("mysql:host=".$host.";dbname=".$name,$user,$pass);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$databases = $pdo->query('SHOW DATABASES')->fetchAll(PDO::FETCH_ASSOC);
+$dbBanned = array('information_schema','mysql','sys','performance_schema');
+
+
+foreach($databases as $database){
+    if (!in_array($database['Database'],$dbBanned)){
+        echo "<h1>".$database['Database']."</h1>";
+        $tables = $pdo->query('SHOW TABLES from '.$database['Database'])->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($tables as $table){
+            $t = $table["Tables_in_".$database['Database']];
+            echo '<br><h3>'.$t.'</h3>';
+            $columns = $pdo->query("SELECT * from ".$database['Database'].".".$t)->fetchAll(PDO::FETCH_ASSOC);
+            $fields = $pdo->query('DESCRIBE '.$database['Database'].'.'.$t)->fetchAll(PDO::FETCH_ASSOC);
+            $co = [];
+            foreach ($fields as $field){
+                array_push($co,$field['Field']);
+            }
+            
+            foreach ($columns as $column){
+                foreach ($co as $c){
+                    echo $column[$c]."<br>";
+                }                
+            }
+        }
+    }
+    
+    
+}
+?>
+<html>
+<div id="output">
+
+</div>
+
+</html>
